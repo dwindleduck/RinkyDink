5135102d2efce3ece648557c36f0f8f51caea731
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2024-04-23 16:18:37 -0400

    Firebase analytics and auth pods both installed in podfile.lock

--- ios/RinkyDink/AppDelegate.mm
+++ ios/RinkyDink/AppDelegate.mm
@@ -2,10 +2,15 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Configure Firebase 
+  [FIRApp configure]; 
+
   self.moduleName = @"RinkyDink";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.

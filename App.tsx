609fa709cdbef5f64bf1ca6d4dd335819b355a44
dwindleduck <119227220+dwindleduck@users.noreply.github.com>
2024-04-23 17:04:38 -0400

    configured android to access firebase app

--- App.tsx
+++ App.tsx
@@ -9,17 +9,15 @@ import React from 'react';
 import {Text} from 'react-native';
 
 // TODO: import firebase pods here
-// this pod is not found, nor listed in package.json
-// .....
-// import auth from '@react-native-firebase/auth';
 
 function App(): React.JSX.Element {
-  const user = {email: 'test@email.com'};
+  const user = null;
   // const user = auth().currentUser;
-  // if (!user) {
-  //   return <Text>Please login</Text>;
-  // }
-  return <Text>Welcome {user.email}</Text>;
+  if (!user) {
+    return <Text>Please login</Text>;
+  }
+  return <Text>Welcome {user}</Text>;
+  // return <Text>Welcome {user.email}</Text>;
 }
 
 export default App;
